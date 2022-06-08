83af15fea0747116b7d4065397798b67f9492159
CaptainLazarus <adityagudimetla@gmail.com>
2022-06-09 03:34:31 +0530

    Finished Util Module

--- src/utils/Util.ts
+++ src/utils/Util.ts
@@ -10,4 +10,7 @@ export class Util {
     public static llequal(a: any[] , b: any[]) {
         return (a.length <= b.length);
     }
+    public static logStep() {
+        console.log("\n----------------------------------------------------------------------\n");
+    }
 }
