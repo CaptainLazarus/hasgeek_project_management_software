153db27d16919b83c411ba2f005819ce7c390ea4
CaptainLazarus <adityagudimetla@gmail.com>
2022-06-09 03:34:55 +0530

    Finished MVP. Add features

--- src/index.ts
+++ src/index.ts
@@ -3,56 +3,93 @@ import { Emails } from "./utils/Email";
 import express, { Express } from "express"
 import bodyParser from "body-parser";
 import multer, { Multer } from 'multer';
-import {Util} from "./utils/Util";
+import { Util } from "./utils/Util";
+import console from "console";
+import fs from "fs";
+import { parse } from "csv";
 
 const app: Express = express();
 const PORT: Number | String = process.env.PORT || 9010;
-const upload: Multer = multer({ dest: 'uploads/' })
+const upload: Multer = multer({ dest: 'src/uploads/' })
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(upload.array("csvFile" , 1)); 
+app.use(express.static('public'));
 
 const EMAIL: string = (typeof process.env.EMAIL === 'undefined') ? ("") : process.env.EMAIL;
 const PASSWORD: string = (typeof process.env.PASSWORD === 'undefined') ? ("") : process.env.PASSWORD;
 let email: Emails | null;
 
 if (EMAIL === "" || PASSWORD === "") {
-  console.log(`\n\nPlease Configure Email and Password, then restart application. 
+      console.log(`\n\nPlease Configure Email and Password, then restart application. 
   \nTo add username/password, create a .env folder at root and add them as shown
   \n\tEMAIL = 'email'
   \n\tPASSWORD = 'password'
   \n\n If you're using an app password, put it in the password field.`);
 }
 else {
-  app
-    .post("/" , (req: any , res: any) => {
-      email = new Emails(EMAIL, PASSWORD);
+      app.locals.email = new Emails(EMAIL, PASSWORD);
+      app.post("/", upload.single("send_to"), (req: any, res: any) => {
+            // ----------------------------------------------------------------------------------------------------------------------------
+            // Declaring Variables.
 
-// ----------------------------------------------------------------------------------------------------------------------------
-      
-      let text: string;
-      let varNames: string[]
-      let inputValues: string[][]
-      let output: string[] = [];
-      let email_to: string[] = [];
+            let text: string;
+            let subject: string;
+            let varNames: string[]
+            let inputValues: string[][] = [];
+            let output: string[] = [];
+            let email_to: any[] = [];
+            let email = app.locals.email;
+            let fileData: string[][] = [];
 
-// ----------------------------------------------------------------------------------------------------------------------------
+            // Reading csv file.
+            const filePath = __dirname+`/uploads/${req.file.filename}`
+            fs.createReadStream(filePath)
+                  .pipe(parse({ delimiter: ",", from_line: 1 }))
+                  .on("data", function (row: any) {
+                        // Pushing csv row by row
+                        fileData.push(row); 
+                  })
+                  .on("end", function () {
+                        // Debug stuff
+                       fs.unlinkSync(filePath);
+                       Util.logStep();
+                       console.log("finished reading csv");
+                       console.log(fileData);
 
-      text = req.body.text || "Hello ${NAME}, how's ${ADDRESS}?"; // Take from user
-      varNames = req.body.text || ["NAME", "ADDRESS"]; // Take from user
-      inputValues = req.body.inputValues || [["aditya", "hyderabad"], ["priya", "gwalior"] , ["asdmnas", "ior" , "asdjansd"]]; // Take from CSV
-    
-// ----------------------------------------------------------------------------------------------------------------------------
+                       // Data Template -> xs:x -> xs = input values ; x = email. Popping email leaves only input values.
+                       // Assingn accordingly
+                       fileData.forEach(row => {
+                             // trim each element of row due to spaces + other nonsense.
+                             row = row.map((e) => e.trim())
+                             email_to.push(row.pop());
+                             inputValues.push(row);
+                       });
 
-      output = email.composeEmail(text , varNames , inputValues);
+                       // Check inside. fn validInput parses through input from POST request and transforms it into object.
+                       // Used to also parse inputValues before shifting into CSV. 
+                       const input = email.validInput(req.body.text, req.body.varNames , req.body.subject);
 
-// ----------------------------------------------------------------------------------------------------------------------------
-      
+                       text = input[0];
+                       subject = input[2];
+                       varNames = input[1];
+                       
+                       // Creates final output email. No idea how it looks in mail.
+                       output = email.composeEmail(text, varNames, inputValues);
+                       console.log(output);
+                       console.log(email_to);
 
-      for(let i=0 ; i < email_to.length ; i++){
-            console.log(i);
-      }
-      console.log(output);
-    
-    })
-    .listen(PORT , () => {console.log(`Listening on port ${PORT} \n Check http://localhost:9010/`)});
+                       email.sendEmails(EMAIL , email_to, subject, output);
+
+                       // Just for debugging purposes. Should probably send a 200 code.
+                       var final_output = email_to.map((e,i) => [e,output[i]]);
+                       res.send(final_output);
+                        
+                  })
+                  .on("error", function (error) {
+                        console.log(error.message);
+                  });
+      });
+      app.listen(PORT, () => { console.log(`Listening on port ${PORT} \n Check http://localhost:9010/`) });
 }
